6aa170c44148b6bcc37685fa2f73593c001368e2
mcdowellalex <alexmichaelmcdowell@gmail.com>
2020-11-23 14:24:57 +0000

    cleaned global variables file

--- globalVariables.js
+++ globalVariables.js
@@ -3,28 +3,3 @@
 var color = '#2c940c'
 export {color}
 
-
-
-
-
-
-
-
-
-//to export a stylesheet -----------
-
-// var React = require('react-native');
-
-// var {
-//   StyleSheet,
-// } = React;
-
-// module.exports = StyleSheet.create({
-
-// alwaysred: {
-//     backgroundColor: 'red',
-//     height: 100,
-//     width: 100,
-// },
-
-// });
